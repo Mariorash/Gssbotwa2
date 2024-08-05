e1a329329097c684333dbab5da302c242703b801
Mariorash <138030694+Mariorash@users.noreply.github.com>
2024-08-05 04:56:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "ŘĶ|⚜|";
-global.owner = ['254756823943'];
-global.premium = ['254756823943'];
+global.ownername = "Mint Wallpapers";
+global.owner = ['254716663965'];
+global.premium = ['254716663965'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
